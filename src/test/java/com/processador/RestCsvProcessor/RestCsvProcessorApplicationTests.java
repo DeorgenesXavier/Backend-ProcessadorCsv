fb3d249491185e5688d9f8ec0abc6e45171b9781
Deorgenes <deorgenes_souza@sicredi.com.br>
2020-12-22 20:45:34 -0300

    Commit inicial

---
+++ src/test/java/com/processador/RestCsvProcessor/RestCsvProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processador.RestCsvProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCsvProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
